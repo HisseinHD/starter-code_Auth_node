017c033006fb058091e18fcc4b91af6ee837ef51
HisseinHD <hisseindaoud6627@gmail.com>
2025-08-04 17:34:39 +0200

    modiffication du usercontroller precisement profil

--- controllers/userController.js
+++ controllers/userController.js
@@ -347,7 +347,8 @@ const resetPassword = async (req, res) => {
 // Get User Profile
 const getProfile = async (req, res) => {
   try {
-    const userId = req.userData.userId;
+    const userId = req.user._id;
+    console.log(userId);
     const user = await userModel.findById(userId).select("-password");
 
     if (!user) {
@@ -369,9 +370,21 @@ const getProfile = async (req, res) => {
 // Update Profile
 const updateProfile = async (req, res) => {
   try {
-    const userId = req.userData.userId;
+    const userId = req.user._id;
     const { name, email } = req.body;
 
+    // Vérifier si l’e-mail est déjà pris par un autre utilisateur
+    if (email) {
+      const existingUser = await userModel.findOne({ email });
+
+      if (existingUser && existingUser._id.toString() !== userId.toString()) {
+        return res.status(400).json({
+          message: "Cet email est déjà utilisé par un autre utilisateur.",
+        });
+      }
+    }
+
+    // Mettre à jour l'utilisateur
     const user = await userModel
       .findByIdAndUpdate(
         userId,
@@ -381,16 +394,16 @@ const updateProfile = async (req, res) => {
       .select("-password");
 
     if (!user) {
-      return res.status(404).send({ message: "Utilisateur non trouvé" });
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
     }
 
-    res.send({
+    res.status(200).json({
       message: "Profil mis à jour avec succès",
       user,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).send({
+    res.status(500).json({
       message: "Erreur lors de la mise à jour du profil",
       error: error.message,
     });
